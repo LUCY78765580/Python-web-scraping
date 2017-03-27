d04d5a87bf8931cd6eeb0b80ccd24f29d0c7aae5
LUCY78765580 <384698960@qq.com>
2017-03-27 09:12:20 +0800

    教务成绩爬虫

--- JWCJ.py
+++ JWCJ.py
@@ -13,19 +13,18 @@ formData={'zjh':'2014141431216','mm':'xxxxxx'}
 s=requests.Session()
 Post=s.post(url='http://zhjw.scu.edu.cn/loginAction.do',data=formData)
 print Post.status_code
-#首先用cookie获取基本信息
+#获取基本信息
 detailURL='http://zhjw.scu.edu.cn/gradeLnAllAction.do?type=ln&oper=qbinfo&lnxndm=2016-2017%D1%A7%C4%EA%C7%EF(%C1%BD%D1%A7%C6%DA)'
-html=s.get(url=detailURL)
+html=requests.get(url=detailURL)
 main=html.content.decode('gbk')
 soup=BeautifulSoup(main,'lxml')
 content=soup.find_all('td',align="center")
-#将信息整理放入一个list中
-#list中创建new_list(方便后续存入excel)
+#将信息放入一个list中,创建new_list(方便后续存入excel)
 data_list=[]
 for data in content:
     data_list.append(data.text.strip())
 new_list=[data_list[i:i+7] for i in range(0,len(data_list),7)]
-#将信息存入excel表格
+#数据存入excel表格
 book=xlwt.Workbook()
 sheet1=book.add_sheet('sheet1',cell_overwrite_ok=True)
 heads=[u'课程号',u'课序号',u'课程名',u'英文课程名',u'学分',u'课程属性',u'成绩']
